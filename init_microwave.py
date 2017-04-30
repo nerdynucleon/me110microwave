b0bc23b03fabaa9fcfb8e74363e01d4297614c39
nerdynucleon <alexychong@gmail.com>
2017-04-30 23:29:58 +0000

    added git pull code to raspberry pi initialization

--- init_microwave.py
+++ init_microwave.py
@@ -9,3 +9,15 @@ for i in range(2,28):
 	GPIO.output(i, GPIO.LOW)
 
 print('All output pins set to zero.')
+
+import socket
+import os
+
+while(True):
+	try:
+		socket.setdefaulttimeout(3)
+		socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+		os.system('git --git-dir=/home/pi/me110microwave/.git pull')
+		break
+	except Exception as err:
+		print(ex.message)
