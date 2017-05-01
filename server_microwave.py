95a89e6f437459f5241f85df81a224c34c5cde50
Alexandre Chong <alexychong@gmail.com>
2017-04-30 17:20:32 -0700

    fix dictionary

--- server_microwave.py
+++ server_microwave.py
@@ -7,35 +7,38 @@ TCP_IP = '107.170.224.107'
 TCP_PORT = 443
 BUFFER_SIZE = 1024
 
-def flip_GPIO(pin_num):
+gpio_func = {
+  'num0' : 24, 
+  'num1' : 17, 
+  'num2' : 4,  
+  'num3' : 11, 
+  'num4' : 6,  
+  'num5' : 26, 
+  'num6' : 10, 
+  'num7' : 27, 
+  'num8' : 14, 
+  'num9' : 8,  
+  'sec30orstart' : 16, 
+  'reheat' : 19, 
+  'kitchentimer' : 13, 
+  'clearorstop' : 12, 
+  'beverage' : 5, 
+  'clock' : 7, 
+  'frozen_vegetable' : 25, 
+  'power' : 9, 
+  'popcorn' : 23, 
+  'timecook' : 22, 
+  'potato' : 18, 
+  'timedefrost' : 15, 
+  'pizza' : 3, 
+  'weightdefrost' : 2
+}
+
+def flip_GPIO(func):
+    pin_num = gpio_func[func]
     GPIO.output(pin_num, GPIO.HIGH)
     time.sleep(0.5)
     GPIO.output(pin_num, GPIO.LOW)	
-
-gpio_func = {'num0' : 24, /
-             'num1' : 17, /
-             'num2' : 4,  /
-             'num3' : 11, /
-             'num4' : 6,  /
-             'num5' : 26, /
-             'num6' : 10, /
-             'num7' : 27, /
-             'num8' : 14, /
-             'num9' : 8,  /
-             'sec30orstart' : 16, /
-             'reheat' : 19, /
-             'kitchentimer' : 13, /
-             'clearorstop' : 12, /
-             'beverage' : 5, /
-             'clock' : 7, /
-             'frozen_vegetable' : 25, /
-             'power' : 9, /
-             'popcorn' : 23, /
-             'timecook' : 22, /
-             'potato' : 18, /
-             'timedefrost' : 15, /
-             'pizza' : 3, /
-             'weightdefrost' : 2}
 
 # Initialize
 GPIO.setmode(GPIO.BCM)
@@ -54,7 +57,7 @@ while(1):
         	attempts = 1
 while(1):
 	data = s.recv(BUFFER_SIZE)
-	print "received data:", data
-	flip_GPIO(sec30orstart)
+	print "recv:", data
+	flip_GPIO(data)
 
 s.close()
